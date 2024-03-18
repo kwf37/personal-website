4127414e78c5eed601e0e48ef9382b322937507f
Kenneth Fang <fangkw42@gmail.com>
2024-03-18 02:31:35 -0400

    Fix merge conflicts

--- src/app.tsx
+++ src/app.tsx
@@ -6,15 +6,9 @@ import { Suspense } from "solid-js";
 export default function App() {
   return (
     <Router
-<<<<<<< HEAD
       root={(props) => (
         <MetaProvider>
           <Title>Ken's Website</Title>
-=======
-      root={props => (
-        <MetaProvider>
-          <Title>SolidStart - with Vitest</Title>
->>>>>>> e790a4c (Feat: Initialize SoiidJS project (#2))
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
 import { PageBase } from "~/components/PageBase";
-=======
-import Counter from "~/components/Counter";
->>>>>>> e790a4c (Feat: Initialize SoiidJS project (#2))
 import "./index.css";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <PageBase>
       <h1>Hello world!</h1>
     </PageBase>
-=======
-    <main>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-    </main>
->>>>>>> e790a4c (Feat: Initialize SoiidJS project (#2))
   );
 }
