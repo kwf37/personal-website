7cddd9b8e3bfe578c8aaba4d7152e376956430ca
kwf37 <fangkw42@gmail.com>
2024-04-07 02:19:21 -0400

    feat/Dialog styling: Style the dialog (#10)

--- src/components/PostDialog.tsx
+++ src/components/PostDialog.tsx
@@ -1,8 +1,14 @@
 import { onMount, type Component, type JSX } from "solid-js";
-import "./PostDialog.module.css";
+import styles from "./PostDialog.module.css";
 import type { CollectionEntry, InferEntrySchema } from "astro:content";
 import { navigate } from "astro:transitions/client";
 import { Logo } from "./Logo";
+import "@fontsource-variable/open-sans";
+import "@fontsource/average-sans";
+
+const dateString = (start: Date, end: Date) => {
+  return `${start.getMonth()}/${start.getFullYear()}-${end.getMonth()}/${end.getFullYear()}`;
+};
 
 export const PostDialog: Component<{
   frontmatter: InferEntrySchema<"software">;
@@ -20,15 +26,35 @@ export const PostDialog: Component<{
   };
 
   return (
-    <dialog ref={dialogRef} onCancel={cancelHandler}>
-      <div>
-        <Logo
-          src={props.frontmatter.logo.src}
-          alt={props.frontmatter.logoAlt}
-        ></Logo>
-        <h2>{props.frontmatter.title}</h2>
-        <h3>{props.frontmatter.company}</h3>
+    <dialog ref={dialogRef} onCancel={cancelHandler} onClick={cancelHandler}>
+      <div
+        class={styles.container}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <span class={styles.header}>
+          <Logo
+            src={props.frontmatter.logo.src}
+            alt={props.frontmatter.logoAlt}
+          ></Logo>
+          <div class={styles["title-container"]}>
+            <h3>{props.frontmatter.title}</h3>
+            <span class={styles.subtitle}>
+              <h4>{props.frontmatter.company}</h4>
+              <h4>
+                {dateString(
+                  props.frontmatter.startDate,
+                  props.frontmatter.endDate
+                )}
+              </h4>
+            </span>
+          </div>
+        </span>
         {props.children}
+        <a href="/">
+          <h4>Back</h4>
+        </a>
       </div>
     </dialog>
   );
