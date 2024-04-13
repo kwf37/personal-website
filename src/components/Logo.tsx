026858b6d1978e4ec75c59b563dd7df199ba2adb
Kenneth Fang <fangkw42@gmail.com>
2024-04-13 02:42:15 -0400

    Enhancement/logo button styling (#18)
    * Added logo button styling
    
    * Remove empty css file
    
    * Revert "Remove empty css file"
    
    This reverts commit 82d61ee5b898e1826d56ef35610a57242c34ec39.
    
    * Remove empty css file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
 import type { Component, JSX } from "solid-js";
-import "./Logo.module.css";
 
 export const Logo: Component<{
   src: string;
   alt: string;
-}> = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+  size: string;
+}> = ({ src, alt, size }) => {
+  return <img src={src} alt={alt} width={size} height={size} />;
 };
