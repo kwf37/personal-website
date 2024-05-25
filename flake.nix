023fcd9c280171a0cd54d5285b4045850049c27d
Kenneth Fang <fangkw42@gmail.com>
2024-05-25 02:02:17 -0400

    Remove vscode from nix flake

--- flake.nix
+++ flake.nix
@@ -34,24 +34,10 @@
           name = "Frontend development shell";
 
           packages = with pkgs; 
-            let vscodeWithExtensions =
-              vscode-with-extensions.override {
-                vscodeExtensions = with vscode-extensions; [
-                  bbenoist.nix
-                  vscodevim.vim
-                  dbaeumer.vscode-eslint
-                  esbenp.prettier-vscode
-                  formulahendry.auto-rename-tag
-                  yzhang.markdown-all-in-one
-                  astro-build.astro-vscode
-                ];
-              };
-            in
-              [ 
-                vscodeWithExtensions 
-                nodejs-18_x
-                nodePackages.pnpm
-              ];
+            [ 
+              nodejs-18_x
+              nodePackages.pnpm
+            ];
           };
       });
     };
